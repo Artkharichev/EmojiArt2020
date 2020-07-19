b82a34f225dce771704ce5302eaabdd6df6dfa17
Artem Kharichev <artkharichev@mail.ru>
2020-07-19 16:13:36 +0300

    add readme

--- EmojiArt2020/EmojiArt.swift
+++ EmojiArt2020/EmojiArt.swift
@@ -7,6 +7,22 @@
 //
 //  Model
 
+
+/*
+EmojiArt - App for downloading images from internet and placing emoji on it.
+
+Developed using:
+
+- MVVM Model
+- SwiftUI, Combine
+- JSON
+- URLSession
+- Gesture
+- Binding, EnvironmentObject, State, Published etc.
+- extension
+- NavigationView ( with button, alert etc.)
+ */
+
 import Foundation
 
 struct EmojiArt: Codable {
